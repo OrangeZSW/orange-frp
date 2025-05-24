940a18616160aa05e02852932330b5e44e180f7e
orange <zsw3166483606@163.com>
2025-05-24 22:55:43 +0800

    task

--- frpc-admin/src/util/router/router.js
+++ frpc-admin/src/util/router/router.js
@@ -3,6 +3,7 @@ import Home from "../../view/Home.vue";
 import Edit from "../../components/Edit.vue";
 import Log from "../../components/Log.vue";
 import scheduledTasks from "../utils/scheduled.js";
+import {fr} from "element-plus/es/locale/index";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,10 +28,18 @@ const router = createRouter({
     ],
 });
 
-
+// 路由切换前
 router.beforeEach((to, from, next) => {
-    scheduledTasks().clear()
-    next();
+    //如果是原地刷新，则不清除，如果是跳转，在跳转之前清除
+    if (from.name !== undefined && to.name !== from.name) {
+        scheduledTasks().clear()
+    }
+    next()
 });
+
+// 路由切换后
+router.afterEach((to, from) => {
+
+})
 
 export default router;
--- frpc-admin/src/util/utils/scheduled.js
+++ frpc-admin/src/util/utils/scheduled.js
@@ -1,24 +1,29 @@
-const scheduledTasks = () => {
-    const tasks = [];
+import {reactive} from 'vue'; // 如果是 Vue 3
 
+const tasks = reactive([]);
+
+const scheduledTasks = () => {
     const add = (task, time) => {
-        tasks.push({task, time, running: false});
-        setInterval(() => {
-            if (!tasks.running) {
-                tasks.running = true;
-                task();
-                tasks.running = false;
-            }
+        // 使用任务的字符串表示（或者任务的唯一标识符）来判断任务是否已存在
+        if (tasks.some((t) => t.task.name === task.name)) {
+            console.log("Task already added.");
+            return;
+        }
+        const intervalId = setInterval(() => {
+            task();
         }, time);
+        tasks.push({task, intervalId});
+        console.log("Tasks {}.", tasks)
     };
 
     const clear = () => {
-        tasks.forEach((task) => {
-            clearInterval(task.intervalId);
+        tasks.forEach(({intervalId}) => {
+            clearInterval(intervalId);
         });
+        tasks.length = 0; // 清空任务队列
     };
 
-    return {add, clear};
+    return {add, clear, tasks};  // 返回 tasks，方便外部访问
 };
 
 export default scheduledTasks;
